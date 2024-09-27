f063260efcc989d85f6a7eae9a1af94ab0a9ed27
Calli Herrmann <calliherrmann@gmail.com>
2024-09-27 17:35:24 -0400

    Add useEffect for displaying flagged one one char at a time

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 
 const App = () => {
   const [flag, setFlag] = useState('')
+  const [displayedFlag, setDisplayedFlag] = useState('');
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -16,13 +17,32 @@ const App = () => {
     produceFlag()
   }, []);
 
+  useEffect(() => {
+    if (!loading && flag) {
+      let currentIndex = -1;
+
+      const intervalId = setInterval(() => {
+        setDisplayedFlag((prev) => prev + flag[currentIndex]);
+        currentIndex++;
+
+        if (currentIndex === flag.length -1) {
+          clearInterval(intervalId);
+        }
+      }, 500); 
+
+      return () => clearInterval(intervalId);
+    }
+  }, [loading, flag]);
+
   return (
     <div>
       {loading ? (
         'Loading...'
       ) : (
         <ul>
-          {flag}
+          {displayedFlag.split('').map((char, i) => (
+            <li key={i}>{char}</li>
+          ))}
         </ul>
       )}
     </div>
@@ -30,3 +50,13 @@ const App = () => {
 };
 
 export default App;
+
+/**
+ * Script used to get URL from step 2:
+ * let allChar = document.querySelectorAll("code > div > span > i.char");
+ * let allCharArray = Array.from(allChar);
+ * let valuesUrl = allCharArray.map((element) => {
+    return element.getAttribute("value");
+  });
+ * valuesUrl.join("")
+*/
